0303acd52c45bb2b9fc9808a8333b7cec8841b8b
Daniel Iyiola <iyiola_daniel@yahoo.com>
2022-12-12 14:56:17 +0100

    getting started with OOP

---
+++ exercises/defineAClass.py
@@ -0,0 +1,21 @@
+class House:
+    '''
+    This is a stub for a class representing a house that can be used to create objects 
+    and evaluate different metrics that we may require in constructing it.
+    '''
+    num_rooms = 5
+    bathrooms = 2
+
+    def cost_evaluation(self, rate):
+        cost = rate * self.num_rooms
+        return cost
+
+#Getting costs for my house
+#1. Create my instance of the House class
+myHouse = House()
+
+#2. Update the number of rooms available in my house
+myHouse.num_rooms = 2
+
+#3. Calculate the cost evaluation given the rate of 5k per room
+print(myHouse.cost_evaluation(5000))
