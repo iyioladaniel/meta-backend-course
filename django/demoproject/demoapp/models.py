a1ab99409b4ea11f53e013f47dd39f0703a389e8
Daniel Iyiola <iyiola_daniel@yahoo.com>
2023-10-01 18:57:48 +0100

    learning migrations.

--- django/demoproject/demoapp/models.py
+++ django/demoproject/demoapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class college(models.Model):
+    CollegeID = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length= 50)
+    strength = models.IntegerField()
+    website = models.URLField()
+
+class Principal(models.Model):
+    CollegeID = models.OneToOneField(college, on_delete=models.CASCADE)
+    Qualification = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+class Subject(models.Model):
+    Subjectcode = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    credits = models.IntegerField()
+
+class Teacher(models.Model):
+    TeacherID = models.IntegerField(primary_key=True)
+    subjectcode = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    Qualification = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
