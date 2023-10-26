3d8a746d44339560a70596d971e6253004e40e73
Daniel Iyiola <iyiola_daniel@yahoo.com>
2023-10-26 22:26:16 +0100

    Just saving my work and updating.

--- django/demoproject/demoproject/views.py
+++ django/demoproject/demoproject/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse, HttpResponseNotFound
 
 def handler404(request, exception):
-    return HttpResponse("404: Page not Found! <br><br> <button onclick='' href='';''> Go to Homepage")
+    return HttpResponse("404: Page not Found! \
+                        <br><br> <button onclick='' href='';''> Go to Homepage")
 
 def home(request):
     return HttpResponseNotFound("404: Page not Found!")
