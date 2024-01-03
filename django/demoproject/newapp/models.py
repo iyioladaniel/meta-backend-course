dbb3fee65ab517cfd4110dff233adfdb34f2611f
Daniel Iyiola <iyiola_daniel@yahoo.com>
2024-01-04 00:48:02 +0100

    added migrations

--- django/demoproject/newapp/models.py
+++ django/demoproject/newapp/models.py
@@ -2,3 +2,8 @@ from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    person_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
